1a86a563c86b0dd98ac55f7d4bf7d2dd9376b841
KH <kahlia.hogg@gmail.com>
2022-01-03 14:06:31 +1100

    Landing Updates

--- src/js/app.js
+++ src/js/app.js
@@ -13,7 +13,7 @@ document.addEventListener("DOMContentLoaded", function () {
         }
       },
       "color": {
-        "value": "#ffffff"
+        "value": "#9eeee1"
       },
       "shape": {
         "type": "polygon",
